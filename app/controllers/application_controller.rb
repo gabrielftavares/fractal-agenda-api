8781f0a8f9ea06385094ba7ec9a07ee2b7893186
gabriel tavares <gabriel29t@gmail.com>
2022-05-16 16:14:54 -0300

    small changes in controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  acts_as_token_authentication_handler_for User, fallback: :none
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
